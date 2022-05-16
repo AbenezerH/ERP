6e62941155ce3a99b68920515c6de73392e7148c
amarech1 <amarechtafere439@gmail.com>
2022-05-16 17:01:40 +0300

    hr database added

--- erpdatabase/index.js
+++ erpdatabase/index.js
@@ -47,6 +47,106 @@ const app=express()
   
 app.get("/table", (req, res) => {
 
+  let sql =
+    `CREATE TABLE admin(admin_id varchar(255),
+     username varchar(255), 
+     ad_email varchar(255), 
+     password varchar(255), 
+     primary key(ad_email))`;
+     db.query(sql, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    let sql1 =
+    `CREATE TABLE organisation(org_name varchar(255),
+     ad_email varchar(255), 
+     location varchar(255), 
+     contact_number varchar(255), 
+     paid_leave_limit int, encashed_leave_limit int,
+      primary key(org_name)) `;
+  db.query((sql,sql1), (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+    let sql2 =
+      `CREATE TABLE department(dept_id varchar(255),
+       dept_name varchar(255), 
+       org_name varchar(255), 
+       primary key (dept_id), foreign key (org_name)
+        references organisation(org_name) on delete set null)`;
+    db.query(sql2, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    let sql3 =
+      `CREATE TABLE gradepay(grade_id varchar(255),
+       grade_name varchar(255), basic_pay int, 
+       grade_pf varchar(255), grade_bonus int, 
+       grade_ta varchar(255), grade_da varchar(255), 
+       primary key (grade_id))`;
+  
+    db.query(sql3, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    let sql4 =
+      `CREATE TABLE employe(present int,  e_id varchar(255),
+        paid_leave_taken int, 
+        encashed_leave_this_month int,
+         encashed_leave_till_date int,
+          doj date,  
+          name varchar(255), dob date, 
+          address varchar(255), city varchar(255),
+           state varchar(255), pincode numeric(6, 0), 
+           ep_email varchar(255) unique, password varchar(255),
+            org_name varchar(255), dept_id varchar(255), 
+            grade_id varchar(255), primary key(ep_email), 
+            foreign key (org_name) references organisation(org_name)
+             on delete  set null, foreign key (dept_id) references 
+             department(dept_id) on delete set null, 
+             foreign key (grade_id) references gradepay(grade_id) 
+             on delete set null)`;
+    db.query(sql4, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    let sql5 =
+    `CREATE TABLE extras(ex_type varchar(255),
+     ex_id varchar(255), primary key (ex_id))`;
+  db.query(sql5, (err) => {
+    if (err) {
+
+      throw err;
+    }
+  });
+    let sql6 =
+      `CREATE TABLE is_given( ex_id varchar(255)
+      , amount int, ep_email varchar(255), 
+      primary key (ex_id, ep_email), foreign key(ep_email)
+       references employe(ep_email) on delete cascade, 
+       foreign key (ex_id) references extras(ex_id) on delete cascade)`;
+    db.query(sql6, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  let sql7 =
+  `CREATE TABLE payroll( transaction_id SERIAL, 
+    month int, year int, gross_pay int, income_tax int, 
+    ep_email varchar(255), ad_email varchar(255), primary key (transaction_id),
+     foreign key(ep_email) references employe(ep_email) on delete set null, 
+     foreign key(ad_email) references admin(ad_email) on delete set null)`;
+  db.query(sql7, (err) => {
+  if (err) {
+    throw err;
+  }
+   });
+
   let sqlBrand = 
   `CREATE TABLE brand
   (id int(11) AUTO_INCREMENT,
@@ -157,170 +257,12 @@ app.get("/table", (req, res) => {
         throw err;
       }
     });
+
+    
     
 });
 
-app.get("/admin", (req, res) => {
 
-  let sql =
-
-    "CREATE TABLE admin(admin_id varchar(255), username varchar(255), ad_email varchar(255), password varchar(255), primary key(ad_email))";
-
-  db.query(sql, (err) => {
-
-    if (err) {
-
-      throw err;
-
-    }
-
-    res.send("admin table created");
-
-  });
-
-});
-
-
-app.get("/organisation", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE organisation(org_name varchar(255), ad_email varchar(255), location varchar(255), contact_number varchar(255), paid_leave_limit int, encashed_leave_limit int, primary key(org_name), foreign key (ad_email) references admin(ad_email) on delete set null)";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("organisation table created");
-  
-    });
-  
-  });
-  
-  app.get("/department", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE department(dept_id varchar(255), dept_name varchar(255), org_name varchar(255), primary key (dept_id), foreign key (org_name) references organisation(org_name) on delete set null)";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("department table created");
-  
-    });
-  
-  });
-
-  app.get("/gradepay", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE gradepay(grade_id varchar(255), grade_name varchar(255), basic_pay int, grade_pf varchar(255), grade_bonus int, grade_ta varchar(255), grade_da varchar(255), primary key (grade_id))";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("gradepay table created");
-  
-    });
-  
-  });
-  
-  app.get("/employe", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE employe(present int, paid_leave_taken int, encashed_leave_this_month int, encashed_leave_till_date int, e_id varchar(255), doj date,  name varchar(255), dob date, address varchar(255), city varchar(255), state varchar(255), pincode numeric(6, 0), ep_email varchar(255) unique, password varchar(255), org_name varchar(255), dept_id varchar(255), grade_id varchar(255), primary key(ep_email), foreign key (org_name) references organisation(org_name) on delete  set null, foreign key (dept_id) references department(dept_id) on delete set null, foreign key (grade_id) references gradepay(grade_id) on delete set null)";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("employe table created");
-  
-    });
-  
-  });
-
-  app.get("/is_given", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE is_given( ex_id varchar(255), amount int, ep_email varchar(255), primary key (ex_id, ep_email), foreign key(ep_email) references employe(ep_email) on delete cascade, foreign key (ex_id) references extras(ex_id) on delete cascade)";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("is_given table created");
-  
-    });
-  
-  });
-
-  app.get("/extras", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE extras(ex_type varchar(255), ex_id varchar(255), primary key (ex_id))";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("extras table created");
-  
-    });
-  
-  });
-  
-  app.get("/payroll", (req, res) => {
-
-    let sql =
-  
-      "CREATE TABLE payroll( transaction_id SERIAL, month int, year int, gross_pay int, income_tax int, ep_email varchar(255), ad_email varchar(255), primary key (transaction_id), foreign key(ep_email) references employe(ep_email) on delete set null, foreign key(ad_email) references admin(ad_email) on delete set null)";
-  
-    db.query(sql, (err) => {
-  
-      if (err) {
-  
-        throw err;
-  
-      }
-  
-      res.send("admin table created");
-  
-    });
-  
-  });
-  
   app.listen('3000', () => {
       console.log('server started at 3000')
   })
