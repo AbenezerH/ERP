e8c8f0550eaf66010db423dee827e7f624c6395f
amarech1 <amarechtafere439@gmail.com>
2022-05-17 00:22:12 +0300

    the duplicate code deleted

--- erpdatabase/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app=express()
-const dbCreator = require('./model/createDatabase')
-
-  dbCreator.createDatabase()
-
-  app.listen('3000', () => {
-      console.log('server started at 3000')
-  })
