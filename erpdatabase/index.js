0127d8205bf14f6ef856eb5eca40f75f74a57bee
amarech1 <amarechtafere439@gmail.com>
2022-05-16 21:53:28 +0300

    database is modified

--- erpdatabase/index.js
+++ erpdatabase/index.js
@@ -47,53 +47,37 @@ const app=express()
   
 app.get("/table", (req, res) => {
 
-  let sql =
+  let admin =
     `CREATE TABLE admin(admin_id varchar(255),
      username varchar(255), 
      ad_email varchar(255), 
      password varchar(255), 
      primary key(ad_email))`;
-     db.query(sql, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-    let sql1 =
+     db.query(admin);
+    let org =
     `CREATE TABLE organisation(org_name varchar(255),
      ad_email varchar(255), 
      location varchar(255), 
      contact_number varchar(255), 
      paid_leave_limit int, encashed_leave_limit int,
       primary key(org_name)) `;
-  db.query((sql,sql1), (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-    let sql2 =
+  db.query(org);
+    let dep =
       `CREATE TABLE department(dept_id varchar(255),
        dept_name varchar(255), 
        org_name varchar(255), 
        primary key (dept_id), foreign key (org_name)
         references organisation(org_name) on delete set null)`;
-    db.query(sql2, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-    let sql3 =
+    db.query(dep);
+    let gradepay =
       `CREATE TABLE gradepay(grade_id varchar(255),
        grade_name varchar(255), basic_pay int, 
        grade_pf varchar(255), grade_bonus int, 
        grade_ta varchar(255), grade_da varchar(255), 
        primary key (grade_id))`;
   
-    db.query(sql3, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-    let sql4 =
+    db.query(gradepay);
+    let employe =
       `CREATE TABLE employe(present int,  e_id varchar(255),
         paid_leave_taken int, 
         encashed_leave_this_month int,
@@ -110,59 +94,40 @@ app.get("/table", (req, res) => {
              department(dept_id) on delete set null, 
              foreign key (grade_id) references gradepay(grade_id) 
              on delete set null)`;
-    db.query(sql4, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-    let sql5 =
+    db.query(employe);
+    let extra =
     `CREATE TABLE extras(ex_type varchar(255),
      ex_id varchar(255), primary key (ex_id))`;
-  db.query(sql5, (err) => {
-    if (err) {
-
-      throw err;
-    }
-  });
-    let sql6 =
+  db.query(extra);
+    let given =
       `CREATE TABLE is_given( ex_id varchar(255)
       , amount int, ep_email varchar(255), 
       primary key (ex_id, ep_email), foreign key(ep_email)
        references employe(ep_email) on delete cascade, 
        foreign key (ex_id) references extras(ex_id) on delete cascade)`;
-    db.query(sql6, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  let sql7 =
+    db.query(given);
+  let payrol=
   `CREATE TABLE payroll( transaction_id SERIAL, 
     month int, year int, gross_pay int, income_tax int, 
     ep_email varchar(255), ad_email varchar(255), primary key (transaction_id),
      foreign key(ep_email) references employe(ep_email) on delete set null, 
      foreign key(ad_email) references admin(ad_email) on delete set null)`;
-  db.query(sql7, (err) => {
-  if (err) {
-    throw err;
-  }
-   });
+  db.query(payrol);
 
   let sqlBrand = 
   `CREATE TABLE brand
   (id int(11) AUTO_INCREMENT,
   name varchar(50),
   PRIMARY KEY(id))
-  `
-
-
+  `;
+  db.query(sqlBrand);
   let sqlCategory =
   `CREATE TABLE category
   (id int(11) AUTO_INCREMENT,
   name varchar(50),
   PRIMARY KEY(id))
-  `
-
-
+  `;
+  db.query(sqlCategory);
   let sqlWarranty =
   `CREATE TABLE warranty
   (id int(11) AUTO_INCREMENT,
@@ -171,9 +136,8 @@ app.get("/table", (req, res) => {
   serial_number varchar(20),
   valid_until datetime,
   PRIMARY KEY(id))
-  `
-
-
+  `;
+  db.query(sqlWarranty);
   let sqlInventory =
     `CREATE TABLE inventory
     (id int(11) AUTO_INCREMENT,
@@ -194,7 +158,7 @@ app.get("/table", (req, res) => {
     FOREIGN KEY (category) REFERENCES category(id) ON DELETE SET NULL, 
     FOREIGN KEY (brand) REFERENCES brand(id) ON DELETE SET NULL,
     FOREIGN KEY (added_by) REFERENCES employe(ep_email) ON DELETE SET NULL)`;
-  
+    db.query(sqlInventory);
   let sqlSales = 
     `CREATE TABLE sales
     (id int(11) AUTO_INCREMENT, 
@@ -209,62 +173,17 @@ app.get("/table", (req, res) => {
     primary key(id),
     FOREIGN KEY (product_id) REFERENCES inventory(id) ON DELETE SET NULL, 
     FOREIGN KEY (warranty) REFERENCES warranty(id) ON DELETE SET NULL,
-    FOREIGN KEY (sold_by) REFERENCES employe(ep_email) ON DELETE SET NULL);`;
-
-
+    FOREIGN KEY (sold_by) REFERENCES employe(ep_email) ON DELETE SET NULL)`;
+    db.query(sqlSales);
     let sqlDamagedGood =
     `CREATE TABLE damaged_good
     (id int(11) AUTO_INCREMENT,
     product_id int(11),
     quantity double,
     PRIMARY KEY (id),
-    FOREIGN KEY(product_id) REFERENCES inventory(id) ON DELETE SET NULL)`
-    db.query(sqlBrand, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-
-    db.query(sqlCategory, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-
-    db.query(sqlWarranty, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-
-    db.query(sqlInventory, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-
-    db.query(sqlSales, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-
-    db.query(sqlDamagedGood, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-    
-    
+    FOREIGN KEY(product_id) REFERENCES inventory(id) ON DELETE SET NULL)`;
+    db.query(sqlDamagedGood);  
 });
-
-
   app.listen('3000', () => {
       console.log('server started at 3000')
   })
