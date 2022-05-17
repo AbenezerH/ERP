7fba90831675abfb695f29ee004e8f4a63ff929f
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-17 05:27:42 +0300

    added the inventory route

--- ERP-backend_development/erpdatabase/index.js
+++ ERP-backend_development/erpdatabase/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app=express()
 const dbCreator = require('./model/createDatabase')
+const inventoryRoute = require('./routes/inventoryRoutes')
 
   dbCreator.createDatabase()
+
+
+  app.use('/inventory', inventoryRoute)
 
   app.listen('3000', () => {
       console.log('server started at 3000')
