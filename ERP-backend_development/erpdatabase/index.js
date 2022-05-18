7072b36456592426d2b38787c1c800fdf849a573
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-18 16:58:08 +0300

    added the brand apis

--- ERP-backend_development/erpdatabase/index.js
+++ ERP-backend_development/erpdatabase/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app=express()
 const dbCreator = require('./model/createDatabase')
+
 const inventoryRoute = require('./routes/inventoryRoutes')
+const brandRoute = require('./routes/brandRoutes')
 
-  dbCreator.createDatabase()
+dbCreator.createDatabase()
 
-  app.use(express.json())
-  app.use('/inventory', inventoryRoute)
+app.use(express.json())
+app.use('/inventory', inventoryRoute)
+app.use('/brand', brandRoute)
 
-  app.listen('3000', () => {
-      console.log('server started at 3000')
-  })
+app.listen('3000', () => {
+    console.log('server started at 3000')
+})
