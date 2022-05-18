456db8f6c1931b6400b88949de1b2e402a91d441
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-18 09:29:15 +0300

    the router connected delete get with id

--- ERP-backend_development/erpdatabase/routes/inventoryRoutes.js
+++ ERP-backend_development/erpdatabase/routes/inventoryRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const inventoryController = require('../controller/inventoryController')
 
 
-router.get('/', inventoryController.getAllInventory)
+router.get('/', inventoryController.dbInventory.getAllInventory)
 
-router.post('/add', inventoryController.addInventory)
+router.post('/add', inventoryController.dbInventory.addInventory)
 
-//router.get('/:id', inventoryController.getInventory)
+router.get('/:id', inventoryController.dbInventory.getInventory)
+
+router.delete('/delete/:id', inventoryController.dbInventory.deleteItem)
 
 module.exports = router;
