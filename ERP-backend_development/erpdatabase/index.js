05e07403fdd0c46cc692ab4d09929fd7ca78d6dc
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-18 17:59:28 +0300

    added warranty apis

--- ERP-backend_development/erpdatabase/index.js
+++ ERP-backend_development/erpdatabase/index.js
@@ -4,12 +4,15 @@ const dbCreator = require('./model/createDatabase')
 
 const inventoryRoute = require('./routes/inventoryRoutes')
 const brandRoute = require('./routes/brandRoutes')
+const warrantyRoute = require('./routes/warrantyRoutes')
 
 dbCreator.createDatabase()
 
 app.use(express.json())
+
 app.use('/inventory', inventoryRoute)
 app.use('/brand', brandRoute)
+app.use('/warranty', warrantyRoute)
 
 app.listen('3000', () => {
     console.log('server started at 3000')
