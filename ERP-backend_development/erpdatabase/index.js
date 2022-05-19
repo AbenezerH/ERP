61ba0d2ecabb90d23ee1780721e815d5eda86515
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-19 05:32:23 +0300

    added sales apis

--- ERP-backend_development/erpdatabase/index.js
+++ ERP-backend_development/erpdatabase/index.js
@@ -5,6 +5,7 @@ const dbCreator = require('./model/createDatabase')
 const inventoryRoute = require('./routes/inventoryRoutes')
 const brandRoute = require('./routes/brandRoutes')
 const warrantyRoute = require('./routes/warrantyRoutes')
+const salesRoute = require('./routes/salesRoutes')
 
 dbCreator.createDatabase()
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use('/inventory', inventoryRoute)
 app.use('/brand', brandRoute)
 app.use('/warranty', warrantyRoute)
+app.use('/sales', salesRoute)
 
 app.listen('3000', () => {
     console.log('server started at 3000')
