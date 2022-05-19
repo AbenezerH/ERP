8e57d5b334a505278d35b1b3b9dd53e126334f00
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-19 06:46:11 +0300

    added liability apis

--- ERP-backend_development/erpdatabase/index.js
+++ ERP-backend_development/erpdatabase/index.js
@@ -6,6 +6,7 @@ const inventoryRoute = require('./routes/inventoryRoutes')
 const brandRoute = require('./routes/brandRoutes')
 const warrantyRoute = require('./routes/warrantyRoutes')
 const salesRoute = require('./routes/salesRoutes')
+const liabilityRoute = require('./routes/liabilityRoutes')
 
 dbCreator.createDatabase()
 
@@ -15,6 +16,7 @@ app.use('/inventory', inventoryRoute)
 app.use('/brand', brandRoute)
 app.use('/warranty', warrantyRoute)
 app.use('/sales', salesRoute)
+app.use('/liability', liabilityRoute)
 
 app.listen('3000', () => {
     console.log('server started at 3000')
