b8c0a69c48469a564422b0b2fc344ae77329b63b
AbenezerH <abenezer.hailu00@gmail.com>
2022-05-26 17:26:37 +0300

    to avoid cross origin access not allowed

--- erpdatabase/index.js
+++ erpdatabase/index.js
@@ -19,6 +19,12 @@ const payrollRoute = require('./routes/payrollRoutes')
 
 dbCreator.createDatabase()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+    
 app.use(express.json())
 
 app.use('/erpdatabase/hr', hrRoute)
