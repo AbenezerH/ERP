5094efafc00c397576ae9a7e51ab82e2fc890111
MickyBe <bmikiyas@gmail.com>
2022-06-12 21:49:04 +0300

    profile wraper

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -2,12 +2,18 @@ import styles from './Profile.module.scss'
 import {useAuthValue} from '../../AuthContext'
 import { signOut } from 'firebase/auth' 
 import { auth } from '../../firebase'
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 
 
 function Profile() {
   const {currentUser} = useAuthValue()
 
   return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
       <div className={styles.center}>
         <div className={styles.profile}>
           <h1>Profile</h1>
@@ -18,6 +24,8 @@ function Profile() {
           </p>
           <span onClick={() => signOut(auth)}>Sign Out</span>
         </div>
+      </div>
+      </div>
       </div>
   )
 }
