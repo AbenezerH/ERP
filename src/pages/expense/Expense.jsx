2c6eb4f30a22b5f9a7f32683cf8dae766cacd1f3
abenih <38100799+abenih@users.noreply.github.com>
2022-06-16 11:41:30 -0700

    modified expense page

--- src/pages/expense/Expense.jsx
+++ src/pages/expense/Expense.jsx
@@ -1,18 +1,18 @@
-import "./expense.scss"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Navbar from "../../components/navbar/Navbar"
-import Datatable from "../../components/datatable/Datatable"
+import "./expense.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import ExpenceDatatable from "../../components/datatable/ExpenceDatatable";
 
 const Expense = () => {
   return (
     <div className="list">
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
-        <Datatable title={"expenses"}/>
+        <Navbar />
+        <ExpenceDatatable title={"expenses"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Expense
+export default Expense;
