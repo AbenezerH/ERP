99f8d1ea144037647ec8795e22af1a9d1a42e61b
abenih <38100799+abenih@users.noreply.github.com>
2022-06-16 11:39:28 -0700

    added expense and sales datatable source

---
+++ src/expencedatatablesource.js
@@ -0,0 +1,28 @@
+const url = "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+export const userColumns = [
+  { field: "id",
+   headerName: "ID", 
+   width: 60 },
+  {
+    field: "name",
+    headerName: "name",
+    width: 120,
+  },
+  {
+    field: "created_at",
+    headerName: "Creation Date",
+    width: 150,
+  },
+  {
+    field: "ex_type",
+    headerName: "Expence Type",
+    width: 130,
+  },
+  {
+    field: "ex_amount",
+    headerName: "Expence Amount",
+    width: 130,
+  },
+  
+  
+];
---
+++ src/salesdatatablesource.js
@@ -0,0 +1,46 @@
+const url = "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+export const userColumns = [
+  { field: "id", 
+  headerName: "ID", 
+  width: 60 },
+ 
+  {
+    field: "product_id",
+    headerName: "Product Id",
+    width: 100,
+  },
+
+  {
+    field: "number_of_items",
+    headerName: "Number Of Items",
+    width: 150,
+  },
+
+  {
+    field: "selling_price",
+    headerName: "Selling Price",
+    width: 100,
+  },
+
+  {
+    field: "warranty",
+    headerName: "Warranty",
+    width: 100,
+  },
+  {
+    field: "vat",
+    headerName: "Vat",
+    width: 100,
+  },
+  {
+    field: "witholding_tax",
+    headerName: "Witholding Tax",
+    width: 150,
+  },
+  {
+    field: "created_at",
+    headerName: "Creation Date",
+    width: 180,
+  },
+  
+];
