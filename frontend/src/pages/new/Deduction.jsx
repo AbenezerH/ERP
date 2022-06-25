a6df98813d0fc4803b62afba9b59e52885423a28
abeba1 <abebamolla2121@gmail.com>
2022-06-25 10:08:58 +0300

    deduction added

--- frontend/src/pages/new/Deduction.jsx
+++ frontend/src/pages/new/Deduction.jsx
@@ -1,52 +1,13 @@
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Deduction = ({ inputs, title }) => {
-  
-    const [formData, setFormData] = useState({
-        id:"",
-      grade_id: " ",
-        grade_name: "", 
-        basic_pay: "", 
-        grade_pf: "", 
-        grade_bonus: "", 
-        grade_ta: "",
-        grade_da: "",
-      });
-
-      
       function fieldChangeHandler(event){
         const target = event.target
         const {name, value} = target
       
-        setFormData(prevData => {
-            return {
-                ...prevData,
-                [name]: value
-            } 
-        })
-      
       }
-      
-      
-      function addgrade(event) {
-        fetch("http://localhost:5000/erpdatabase/hr/addGrade", {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        }).then(res => res.json())
-            .then(data => console.log("add " + data))
-        .catch(err => console.log("error " + err))
-      
-        console.log("Add grade")
-      
-      }
-
-      console.log(formData)
-      
       
   return (
     <div className="new">
@@ -54,26 +15,27 @@ const Deduction = ({ inputs, title }) => {
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>{title}</h1>
+          <h1>Deduction</h1>
         </div>
         <div className="bottom">
           <div className="right">
             <form>
             {inputs.map((input) => (
+                
                 <div className="formInput" key={input.id}>
-                  <label>{input.label}</label>
-                  <input 
-                    type={input.type} 
-                    placeholder={input.placeholder} 
-                    value={formData[input.type]}
-                    name={input.name}
-                    onChange={fieldChangeHandler}/>
+                  <label>{input.label}</label> 
+                    <input 
+                      type={input.type}
+                      placeholder={input.placeholder}
+                      value={input.type}
+                      name={input.name}
+                      onChange={fieldChangeHandler}
+                      required
+                      />
                 </div>
               ))}
-              
-              <button onClick={addgrade}>Send</button>
             </form>
-          </div>
+       </div>
         </div>
       </div>
     </div>
